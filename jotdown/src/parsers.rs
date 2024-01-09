b1dd3dca488671fbc48a28ef37923bc3f482ce26
Dworv <dwarvyt@gmail.com>
2024-01-08 16:40:28 -0800

    ref: standardize how empty lines are handled

--- jotdown/src/parsers.rs
+++ jotdown/src/parsers.rs
@@ -1,22 +1,25 @@
+//! Parsers should follow the following rules:
+//! - Consume all leading whitespace
+//! - Consume no trailing whitespace
+
 use nom::{
     bytes::complete::{take_while1, tag},
-    character::complete::{multispace0, multispace1, not_line_ending, line_ending, u64 as nom_u64},
+    character::complete::{multispace0, multispace1, not_line_ending, line_ending, u64 as nom_u64, space0},
     error::Error,
-    sequence::tuple,
-    Err, IResult, multi::many1, combinator::opt,
+    sequence::{tuple, terminated},
+    Err, IResult, multi::{many1, many0}, combinator::opt,
 };
 
 pub fn parse_paragraph(input: &str) -> IResult<&str, &str> {
-    let (input, _) = multispace0(input)?;
+    let (input, _) = many0(empty_line)(input)?;
     let (input, paragraph) = not_line_ending1(input)?;
     Ok((input, paragraph))
 }
 
 pub fn parse_title_heading(input: &str) -> IResult<&str, (&str, u8)> {
-    let (input, _) = multispace0(input)?;
+    let (input, _) = many0(empty_line)(input)?;
     let (input, (num_hashtags, _, content)) =
         tuple((heading_tag, multispace1, not_line_ending))(input)?;
-    let (input, _) = multispace0(input)?;
     Ok((input, (content, num_hashtags)))
 }
 
@@ -33,7 +36,7 @@ fn heading_tag(input: &str) -> IResult<&str, u8> {
 }
 
 pub fn parse_unordered_list(input: &str) -> IResult<&str, Vec<&str>> {
-    let (input, _) = multispace0(input)?;
+    let (input, _) = many0(empty_line)(input)?;
     let (input, list) = many1(parse_unordered_list_entry)(input)?;
     Ok((input, list))
 }
@@ -45,15 +48,15 @@ fn parse_unordered_list_entry(input: &str) -> IResult<&str, &str> {
 }
 
 pub fn parse_ordered_list(input: &str) -> IResult<&str, Vec<&str>> {
-    let (mut input, _) = multispace0(input)?;
+    let (mut input, _) = many0(empty_line)(input)?;
     let mut list = Vec::new();
     let mut expected_num = 1;
-    while let Ok((input_, (num, entry))) = parse_ordered_list_entry(input) {
+    while let Ok((adj_input, (num, entry))) = parse_ordered_list_entry(input) {
         if num != expected_num {
             break;
         }
         list.push(entry);
-        input = input_;
+        input = adj_input;
         expected_num += 1;
     }
     if list.is_empty() {
@@ -77,4 +80,8 @@ fn not_line_ending1(input: &str) -> IResult<&str, &str> {
         return Err(Err::Error(Error::new(input, nom::error::ErrorKind::NoneOf)));
     }
     Ok((input, paragraph))
+}
+
+fn empty_line(input: &str) -> IResult<&str, &str> {
+    terminated(space0, line_ending)(input)
 }
